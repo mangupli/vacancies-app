deeb64d2aac2bfaadb7ee3f0eb5d7f2a3182834b
mangupli <elizaveta.mangupli@gmail.com>
2023-11-20 16:16:35 +0300

    check and create folder for avatars

--- server/app.js
+++ server/app.js
@@ -1,14 +1,20 @@
 require('@babel/register');
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const serverConfig = require('./config/serverCofnig');
 const indexRouter = require('./routes/index.routes');
 const checkUser = require('./middlewares/verifyJWT');
 
+const checkAndCreateFolder = require('./utils/checkAndCreateFolder');
+
 const app = express();
 
 serverConfig(app);
+
+// создание папки для сохранения фотографий пользователя
+checkAndCreateFolder(path.join(__dirname, 'public', 'img', 'avatars'))
 
 // проверка JWT токенов
 app.use(checkUser);
